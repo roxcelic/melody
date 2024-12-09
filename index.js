0a6279ba808ae2cdb7ba382f9a2d1326029d66aa
roxcelic <roxtheangel@outlook.com>
2024-12-09 20:00:00 +0000

    forgot to import it

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
-const fs = require('fs');
 const express = require('express');
+const cors = require('cors');
 
 const { getstatus, getimage } = require('./utils/utils');
 const { getCurrentlyPlaying, getPlaylists } = require('./utils/spotify_utils');
