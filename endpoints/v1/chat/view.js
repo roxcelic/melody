47c46576d4aa445f89a30f3b935eff050fcc35e1
mushy <157179472+roxcelic@users.noreply.github.com>
2025-04-08 18:53:45 +0100

    another quick fix

--- endpoints/v1/chat/view.js
+++ endpoints/v1/chat/view.js
@@ -3,7 +3,7 @@ const utils = require('../../../utils');
 const router = express.Router();
 
 router.get('/chat/view', async (req, res) => {
-    let chat = utils.readDataFile("chat");
+    let chat = await utils.readDataFile("chat");
 
     res.json({
         chat
