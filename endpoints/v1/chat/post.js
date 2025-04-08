fa67b2ca4f36539336b2eaab8a025c6e75ed4c11
mushy <157179472+roxcelic@users.noreply.github.com>
2025-04-08 18:52:02 +0100

    a few quick fixs

--- endpoints/v1/chat/post.js
+++ endpoints/v1/chat/post.js
@@ -3,9 +3,11 @@ const utils = require('../../../utils');
 const router = express.Router();
 
 router.post('/chat/post', async (req, res) => {
-    let {text} = req.body;
+    let text = req.body;
 
-    let chat = utils.readDataFile("chat") || [];
+    let chat = await utils.readDataFile("chat");
+    chat = chat == "empty" ? [] : chat;
+
     chat.push([text.upload || "", text.color || "#fff"]);
 
     if (chat.length > 99) chat.length = 99;
