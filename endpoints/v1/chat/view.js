d440ea63660dee933e11f812bf1efa45e3204a50
mushy <157179472+roxcelic@users.noreply.github.com>
2025-04-30 22:24:02 +0100

    test

--- endpoints/v1/chat/view.js
+++ endpoints/v1/chat/view.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 router.get('/chat/view', async (req, res) => {
     let chat = await utils.readDataFile("chat");
 
+    console.log(req.query.chatName == "admin" );
+    console.log(!utils.IsAdmin(req, res) );
     if (req.query.chatName == "admin" && !utils.IsAdmin(req, res)){
         res.json({"status": "evil do-er"});
     } else {
