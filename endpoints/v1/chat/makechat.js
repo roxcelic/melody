6a5ccc64d98604ea774921ea76b1f2747f2605ff
mushy <157179472+roxcelic@users.noreply.github.com>
2025-05-06 23:18:15 +0100

    roxie stupidity fail safe

--- endpoints/v1/chat/makechat.js
+++ endpoints/v1/chat/makechat.js
@@ -4,15 +4,19 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get('/chat/makechat', utils.myLogger, async (req, res) => {
-    if (req.query.chatName) {
-        let folder = await utils.makefolder(`chat`);
-
-        if (!fs.existsSync(`${folder}/${req.query.chatName}.json`)){
-            utils.writeDataFile(`chat/${req.query.chatName}`, []);
+    try {
+        if (req.query.chatName) {
+            let folder = await utils.makefolder(`chat`);
+    
+            if (!fs.existsSync(`${folder}/${req.query.chatName}.json`)){
+                utils.writeDataFile(`chat/${req.query.chatName}`, []);
+            }
         }
+    
+        res.json({status: "succesfull"});
+    } catch (e) {
+        res.json({status: "un-succesfull"});
     }
-
-    res.json({status: "succesfull"});
 });
 
 module.exports = router;
